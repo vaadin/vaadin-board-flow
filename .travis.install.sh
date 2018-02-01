a0d0acce45ef9bf50e5f580a41a9945c4a0cd72e
Artur Signell <artur@vaadin.com>
2018-02-01 16:56:33 +0200

    Fix Travis builds

--- .travis.install.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-mvn install -Dvaadin.spreadsheet.developer.license=$SPREADSHEET_LICENSE -Dvaadin.charts.developer.license=$CHARTS_LICENSE -Dvaadin.board.developer.license=$BOARD_LICENSE -Dvaadin.testbench.developer.license=$TESTBENCH_LICENSE -DskipTests=true -DskipITs
